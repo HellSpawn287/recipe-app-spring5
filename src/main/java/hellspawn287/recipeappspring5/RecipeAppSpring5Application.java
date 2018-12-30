c222ee16061d3d1e97035569f051c339202dc9b5
Robert Prus <robert.prus87@gmail.com>
2018-12-30 19:11:30 +0100

    Add project files

---
+++ src/main/java/hellspawn287/recipeappspring5/RecipeAppSpring5Application.java
@@ -0,0 +1,14 @@
+package hellspawn287.recipeappspring5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeAppSpring5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeAppSpring5Application.class, args);
+    }
+
+}
+
---
+++ src/test/java/hellspawn287/recipeappspring5/RecipeAppSpring5ApplicationTests.java
@@ -0,0 +1,17 @@
+package hellspawn287.recipeappspring5;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RecipeAppSpring5ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
