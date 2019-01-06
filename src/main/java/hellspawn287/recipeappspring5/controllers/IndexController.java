b65eeffbc10792c5e13930893aac97e1db45b33f
Robert Prus <robert.prus87@gmail.com>
2019-01-07 00:33:16 +0100

    Add One to One JPA

--- src/main/java/hellspawn287/recipeappspring5/controllers/IndexController.java
+++ src/main/java/hellspawn287/recipeappspring5/controllers/IndexController.java
@@ -1,4 +1,4 @@
-package hellspawn287.recipeappspring5.Controllers;
+package hellspawn287.recipeappspring5.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
