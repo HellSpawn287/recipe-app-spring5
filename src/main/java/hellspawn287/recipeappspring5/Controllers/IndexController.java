d26a6769224b23d98f1e23c9790a343c23b14608
Robert Prus <robert.prus87@gmail.com>
2019-01-06 21:54:12 +0100

    Add index page and IndexController

---
+++ src/main/java/hellspawn287/recipeappspring5/Controllers/IndexController.java
@@ -0,0 +1,14 @@
+package hellspawn287.recipeappspring5.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "/index"})
+    public String getIndexPage(){
+        System.out.println("Some msg to say: ...");
+        return "index";
+    }
+}
