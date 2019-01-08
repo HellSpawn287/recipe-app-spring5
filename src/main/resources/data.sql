ca56aceab3bd99fb0d297360324049b818183844
Robert Prus <robert.prus87@gmail.com>
2019-01-08 02:56:07 +0100

    Database initialization with Spring Boot and SQL data source.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO category (description) VALUES ('Japan');
+INSERT INTO category (description) VALUES ('Italian');
+INSERT INTO category (description) VALUES ('Mexican');
+INSERT INTO category (description) VALUES ('Fast Food');
+INSERT INTO unit_of_Measure (description) VALUES ('Teaspoon');
+INSERT INTO unit_of_Measure (description) VALUES ('Tablespoon');
+INSERT INTO unit_of_Measure (description) VALUES ('Cup');
+INSERT INTO unit_of_Measure (description) VALUES ('Pinch');
+INSERT INTO unit_of_Measure (description) VALUES ('Ounce');
