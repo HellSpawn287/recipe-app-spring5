9b3c9a86e90f357a10a4084c17a4e57d1855738d
Robert Prus <robert.prus87@gmail.com>
2019-01-09 04:59:24 +0100

    Add missing Category and Unit of Measures to data.sql Add bootstrap to create recipes on startup Add service layer Change IndexController to use RecipeService instead of repositories Pass recipes list to Thymeleaf view to display on index page Add constructors to Ingredient Add Large object annotation to Recipe's direction property

--- src/main/java/hellspawn287/recipeappspring5/controllers/IndexController.java
+++ src/main/java/hellspawn287/recipeappspring5/controllers/IndexController.java
@@ -1,34 +1,22 @@
 package hellspawn287.recipeappspring5.controllers;
 
-import hellspawn287.recipeappspring5.domain.Category;
-import hellspawn287.recipeappspring5.domain.UnitOfMeasure;
-import hellspawn287.recipeappspring5.repositories.CategoryRepository;
-import hellspawn287.recipeappspring5.repositories.UnitOfMeasureRepository;
+import hellspawn287.recipeappspring5.services.RecipeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Optional;
 
 @Controller
 public class IndexController {
 
-    private CategoryRepository categoryRepository;
-    private UnitOfMeasureRepository unitOfMeasureRepository;
+    private final RecipeService recipeService;
 
-    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
-        this.categoryRepository = categoryRepository;
-        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
     }
 
     @RequestMapping({"", "/", "/index"})
-    public String getIndexPage(){
-        System.out.println("Some msg to say: ...");
-
-        Optional<Category> categoryOptional = categoryRepository.findByDescription("Italian");
-        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Cup");
-
-        System.out.println("Category Id is: " + categoryOptional.get().getId());
-        System.out.println("Unit Of Measure Id is: " + unitOfMeasureOptional.get().getId());
+    public String getIndexPage(Model model){
+        model.addAttribute("recipes", recipeService.getRecipes());
 
         return "index";
     }
---
+++ src/main/java/hellspawn287/recipeappspring5/services/RecipeService.java
@@ -0,0 +1,10 @@
+package hellspawn287.recipeappspring5.services;
+
+import hellspawn287.recipeappspring5.domain.Recipe;
+
+import java.util.Set;
+
+public interface RecipeService{
+
+    Set<Recipe> getRecipes();
+}
