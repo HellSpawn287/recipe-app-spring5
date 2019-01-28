7846c7c8dcf1f25ff168b22252b24d1fec379d6b
Robert Prus <robert.prus87@gmail.com>
2019-01-28 03:21:54 +0100

    Created unit tests for Category, IndexController, RecipeServiceImpl, UnitOfMeasureRepository

---
+++ src/test/java/hellspawn287/recipeappspring5/domain/CategoryTest.java
@@ -0,0 +1,33 @@
+package hellspawn287.recipeappspring5.domain;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CategoryTest {
+
+    Category category;
+
+    @Before
+    public void setUp() {
+        category = new Category();
+    }
+
+    @Test
+    public void getId() throws Exception {
+        Long idValue = 4L;
+
+        category.setId(idValue);
+
+        assertEquals(idValue, (Long) category.getId());
+    }
+
+    @Test
+    public void getDescription() {
+    }
+
+    @Test
+    public void getRecipes() {
+    }
+}
